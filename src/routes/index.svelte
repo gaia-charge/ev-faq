1843d65fe2e3856f258e69c8c9f0348829d24815
Wojtek Siudzinski <admin@suda.pl>
2021-09-28 14:34:58 +0200

    Fit two pages

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,20 @@
-<style>
+<style media="print">
 div {
-    background-color: red;
+    /* The exact 210mm cause the divs to spill into the next page */
+    height: 209.95mm;
+    width: 297mm;
+    background-size: 100% 100%;
+    background: -webkit-linear-gradient(0deg, #daf9f9, #daf9f9) !important;
+}
+h1 {
+    margin: 0;
 }
 </style>
 
 <div>
     <h1>Welcome to SvelteKit</h1>
     <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
+<div>
+    <h1>Page 2</h1>
 </div>
