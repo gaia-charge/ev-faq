ceae18d8f7b21367f247140e08b4a7573506a017
Wojtek Siudzinski <admin@suda.pl>
2021-10-06 14:17:41 +0200

    Finish page 1

--- src/components/Page1.svelte
+++ src/components/Page1.svelte
@@ -5,12 +5,23 @@
 <div>
 	<h1>{$_("title", { default: "Common questions about the electric vehicles"})}</h1>
 	<p class="subtitle">
-	<strong>{$_("subtitle", { default: "Answered by Gaia Green Tech" })}</strong>
+		<strong>{$_("subtitle", { default: "Answered by Gaia Green Tech" })}</strong>
 	</p>
 </div>
 
 <style>
-	div {
-		background-color: red;
+	h1 {
+		position: absolute;
+		top: 3.57cm;
+		left: 21.2cm;
+		font-size: 13mm;
+		line-height: 1.1;
+		width: 80mm;
+	}
+	.subtitle {
+		position: absolute;
+		top: 11.15cm;
+		left: 21.2cm;
+		font-size: 10pt;
 	}
 </style>
