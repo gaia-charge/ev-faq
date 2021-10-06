fccf4167fd75b699ea1c5707652b58d57c8150e4
Wojtek Siudzinski <admin@suda.pl>
2021-10-06 14:08:35 +0200

    Add environment variable for passing language

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,22 +1,38 @@
 <script>
+	import { init, register, isLoading } from 'svelte-i18n';
 	import Page1 from '../components/Page1.svelte';
 	import Page2 from '../components/Page2.svelte';
 	import Page3 from '../components/Page3.svelte';
 	import Page4 from '../components/Page4.svelte';
 	import Page5 from '../components/Page5.svelte';
 	import Page6 from '../components/Page6.svelte';
+	import environment from '../services/environment';
+
+	const fallbackLocale = 'en';
+
+	register('en', () => import('../services/messages/en.json'));
+	register('es', () => import('../services/messages/es.json'));
+	register('pl', () => import('../services/messages/pl.json'));
+	init({
+		fallbackLocale,
+		initialLocale: environment.language || fallbackLocale
+	});
 </script>
 
-<div class="side" id="side-a">
-	<Page5 />
-	<Page6 />
-	<Page1 />
-</div>
-<div class="side" id="side-b">
-	<Page2 />
-    <Page3 />
-    <Page4 />
-</div>
+{#if !$isLoading}
+	<div class="side" id="side-a">
+		<img src="/side-a.svg" class="background" />
+		<!-- <img src="/side-a.png" class="debug" /> -->
+		<Page5 />
+		<Page6 />
+		<Page1 />
+	</div>
+	<div class="side" id="side-b">
+		<Page2 />
+		<Page3 />
+		<Page4 />
+	</div>
+{/if}
 
 <style>
 	/*
@@ -49,7 +65,6 @@
 			width: 307mm;
             height: 220mm;
 			margin: 2em !important;
-			border: 0.5em rgba(0, 0, 0, 0.5) solid;
 		}
 		.background, .debug {
 			width: 307mm;
